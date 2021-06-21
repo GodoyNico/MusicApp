a049e04b3ff5a7f49ca7d4ada65f87719513e736
Nícolas Almeida <nicogodoyalmeida@outlook.com>
2021-06-21 14:21:18 -0300

    início uso json, ajustes viewcontroller

---
+++ MusicChallenge/ViewController/FavoritesListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoritesListViewController.swift
+//  MusicChallenge
+//
+//  Created by Nicolas Godoy on 18/06/21.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    @IBOutlet weak var favoritesTableView: UITableView!
+    
+    var music: Music?
+    var musicService: MusicService?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
